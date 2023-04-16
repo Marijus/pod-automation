77246a34a2a07ef881b76f7bb0f720f59f9034cf
Brandon Chikezie <bchikezie30@gmail.com>
2023-04-16 12:28:18 +0100

    Update timer

--- pod automation store_files/index3.js
+++ pod automation store_files/index3.js
@@ -1,19 +1,14 @@
 const counter = document.querySelector(".counter");
 const digitsContainer = document.querySelector(".digits");
 
-const ONE_DAY = 1000 * 60 * 60 * 24;
-const ONE_HOUR = 1000 * 60 * 60;
-const ONE_MIN = 1000 * 60;
-const ONE_SEC = 1000;
-
 const startDate = new Date(counter?.dataset?.start_date ?? null).getTime();
 const endDate = new Date(counter?.dataset.end_date ?? null).getTime();
 const startCount = +counter?.dataset.start_count ?? 1000;
 const endCount = +counter?.dataset.end_count ?? 2000;
 const frequency = +counter?.dataset.frequency ?? 1000;
 const initDiff = endDate - startDate;
-const maxCountLength = endCount.toFixed(0).length;
 let diff = new Date().getTime() - startDate;
+let maxCountLength = 0;
 
 const blockHeight =
   getComputedStyle(document.body)
@@ -29,53 +24,64 @@ intervalId = setInterval(() => {
     (diff / initDiff) * (endCount - startCount) +
     startCount
   ).toFixed();
+  const currentCountLength = currentCount.length;
 
-  if (count === 0) {
-    for (let i = 0; i < maxCountLength; i++) {
-      const digitBlock = document.createElement("div");
-      digitBlock.classList.add("digits-block");
-      digitsContainer.appendChild(digitBlock);
+  if (currentCountLength > maxCountLength) {
+    // update blocks
+    for (let i = 0; i < currentCountLength; i++) {
+      const isNew = i > maxCountLength - 1;
 
-      if (i % 3 === 0 && i !== maxCountLength - 1) {
+      if (isNew) {
+        const digitBlock = document.createElement("div");
+        digitBlock.classList.add("digits-block");
+        digitsContainer.appendChild(digitBlock);
+
+        const list = document.createElement("div");
+        list.classList.add("slick-list");
+
+        digitBlock.appendChild(list);
+
+        list.style.transform = `translate3d(0px, -${
+          -blockHeight * currentCount[i]
+        }px, 0px)`;
+        list.style.transition = `transform 750ms cubic-bezier(0.645, 0.045, 0.355, 1) 0s`;
+
+        new Array(10)
+          .fill(null)
+          .forEach((_, i) => (list.innerHTML += `<span>${i}</span>`));
+      }
+
+      const nextElement = document.querySelector(
+        `.digits-block:nth-of-type(${i + 1})`
+      )?.nextSibling;
+
+      if (
+        (currentCountLength - (i + 1)) % 3 === 0 &&
+        i !== currentCountLength - 1
+      ) {
+        if (nextElement?.nodeName === "SPAN") continue;
+
         const span = document.createElement("span");
         span.textContent = ",";
-        digitsContainer.appendChild(span);
+
+        nextElement
+          ? digitsContainer.insertBefore(span, nextElement)
+          : digitsContainer.appendChild(span);
+      } else {
+        nextElement?.nodeName === "SPAN" && nextElement.remove();
       }
     }
 
-    for (let i = 0; i < maxCountLength; i++) {
-      const hasNoDigit = !currentCount?.[maxCountLength - (i + 1)];
-      const block = document.querySelector(
-        `.digits-block:nth-of-type(${i + 1})`
-      );
-
-      let list = document.createElement("div");
-      list.classList.add("slick-list");
-
-      block.appendChild(list);
-
-      list.style.transform = `translate3d(0px, -${
-        -blockHeight * +(hasNoDigit ? 0 : currentCount[i])
-      }px, 0px)`;
-      list.style.transition = `transform 750ms cubic-bezier(0.645, 0.045, 0.355, 1) 0s`;
-
-      new Array(10)
-        .fill(null)
-        .forEach((_, i) => (list.innerHTML += `<span>${i}</span>`));
-    }
+    maxCountLength = currentCountLength;
   }
 
-  if (diff > initDiff) {
-    return clearInterval(intervalId);
-  }
+  if (diff > initDiff) return clearInterval(intervalId);
 
   for (let i = 0; i < maxCountLength; i++) {
-    const hasNoDigit = !currentCount?.[maxCountLength - (i + 1)];
-
     document.querySelector(
       `.counter .digits-block:nth-of-type(${i + 1}) .slick-list`
     ).style.transform = `translate3d(0px, ${
-      -blockHeight * +(hasNoDigit ? 0 : currentCount[i])
+      -blockHeight * currentCount[i]
     }px, 0px)`;
   }
 
